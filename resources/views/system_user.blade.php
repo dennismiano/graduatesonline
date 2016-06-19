aad5640a50c66c6cd146bd3aa6db9d389eb80b90
dennis miano <mianodmaina@gmail.com>
2016-06-19 03:55:36 -0700

    additional admin panel styling

--- resources/views/system_user.blade.php
+++ resources/views/system_user.blade.php
@@ -4,7 +4,32 @@
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-3 main-menu">
+			<div class="row">
+				<div class="main-menu-title">
+					<h4 class="main-menu-head">NEW REGISTRATIONS</h4>
+				</div>
+					<div class="col-md-8"><p class="grad-text">Students</p></div><div class="col-md-4"><span class="badge">13</span></div>
+					<div class="col-md-8"><p class="grad-text">Supervisors</p></div><div class="col-md-4"><span class="badge">367</span></div>
+					<div class="col-md-8"><p class="grad-text">Mentors</p></div><div class="col-md-4"><span class="badge">367</span></div>
+			</div>
 			
+			<div class="row">
+				<div class="main-menu-title">
+					<h4 class="main-menu-head">SUPERVISOR APPOINMENTS</h4>
+				</div>
+					<div class="col-md-8"><p class="grad-text">Make new appointments</p></div>
+					<div class="col-md-8"><p class="grad-text">Pending appointments</p></div><div class="col-md-4"><span class="badge">367</span></div>
+					<div class="col-md-8"><p class="grad-text">Active appointments</p></div><div class="col-md-4"><span class="badge">367</span></div>
+			</div>
+			
+			<div class="row">
+				<div class="main-menu-title">
+					<h4 class="main-menu-head">RTC TRACKING</h4>
+				</div>
+					<div class="col-md-8"><p class="grad-text">Faculties</p></div><div class="col-md-4"><span class="badge">367</span></div>
+					<div class="col-md-8"><p class="grad-text">Registered students</p></div><div class="col-md-4"><span class="badge">2178</span></div>
+					<div class="col-md-8"><p class="grad-text">Registered supervisors</p></div><div class="col-md-4"><span class="badge">230</span></div>
+			</div>
 		</div>
 		
 		<div class="col-md-9 grad-content">
